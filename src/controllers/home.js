9c8013127a47a1ecb9e6b9d63214270487296ce2
busybu <gabrielle.fialkoski@hotmail.com>
2023-01-26 09:53:56 -0300

    add: routes and home

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,5 +1,26 @@
 module.exports = {
     async pagInicialGet(req, res) {
         res.render('../views/index');
+    },
+    async pagLogin(req,res){
+        res.render('../views/login-adm')
+    },
+    async pagCadastro(req, res){
+        res.render('../views/cad_adm')
+    },
+    async pagHomeAdm(req, res){
+        res.render('../views/home_adm_master')
+    },
+    async pagDeclaracoesAdm(req, res){
+        res.render('../views/declaracoes_adm')
+    },
+    async pagCriarTurmaAdm(req,res){
+        res.render('../views/criar_turma')
+    },
+    async pagTurmasAdm(req, res){
+        res.render('../views/gerenciar_turma_adm')
+    },
+    async pagUsuariosAdm(req, res){
+        res.render('../views/usuarios_adm')
     }
 }
