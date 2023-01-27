c325c0fe01e17f4c5b1ea14a9ae00d5202c30a89
Emanuelly B. Silva <emanuellybs14@gmail.com>
2023-01-26 21:08:43 -0300

    chega por hj

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,8 @@
 const sequelize = require('sequelize');
 //configurações da base de dados
-const database = new sequelize('Absence_Controller', 'adm_absence_controll', 'etsds10243110',
+const database = new sequelize('Absence_Controll', 'adm_absence_controll', 'etsds10243110',
 {
-dialect: 'mssql', host:'localhost', port: 49775
+dialect: 'mssql', host:'localhost', port: 49675
 });
 
 database.sync();
