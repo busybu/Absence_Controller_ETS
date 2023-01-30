2dc34a1b74a8abf8b37083d273fa82eebdb7e5f2
Emanuelly <emanuellybs14@gmail.com>
2023-01-30 11:18:47 -0300

    tentando mexer com login

--- src/controllers/ControllerAdms.js
+++ src/controllers/ControllerAdms.js
@@ -12,11 +12,11 @@ module.exports = {
             raw: true,
             attributes: ['ID', 'Nome', 'Inicio', 'Fim']
         });
-        const declaracoes = await formulario.findAll({
+        const declaracoe = await formulario.findAll({
             raw: true,
             attributes: ['ID', 'Nome', 'Inicio', 'IdAdmConferiu'],
         });
-        res.render('../views/declaracoes_adm', { turma, id: '', declaracoes });
+        res.render('../views/declaracoes_adm', { turma, id: '', declaracoe });
     },
 
     // ^^-----------------------------------------------------------------------------------------------------------------------------------------
@@ -24,15 +24,15 @@ module.exports = {
     //acessar pag declaracoes conforme turma escolhida
     async postPagDeclaracoes(req, res) {
         const id = req.body.nome;
-        const declaracoes = await formulario.findAll({
+        const declaracoe = await formulario.findAll({
             raw: true,
             attributes: ['ID', 'Nome', 'Inicio', 'IdAdmConferiu'],
             where: { ID: id }
         })
-        const turmas = await turmas.findByPk({ raw: true, attributes: ['ID', 'Nome'] })
+        const turma = await turmas.findByPk({ raw: true, attributes: ['ID', 'Nome'] })
 
 
-        res.render('../views/declaracoes_adm', { turmas, declaracoes, id })
+        res.render('../views/declaracoes_adm', { turma, declaracoe, id })
     },
 
     // ^^-----------------------------------------------------------------------------------------------------------------------------------------
@@ -40,10 +40,13 @@ module.exports = {
     //direcionar para pagina que verifica adm (aceita/edita adm)
     async getAceiteAdm(req, res) {
 
+
         const administradores = await administrador.findAll({
             raw: true,
-            attributes: ['EDV', 'Nome']
+            attributes: ['EDV', 'Nome', 'Ativo'],
         })
+        console.log(administradores)
+
         res.render('../views/usuarios_adm', { administradores })
     },
 
@@ -64,8 +67,12 @@ module.exports = {
             attributes: ['EDV', 'Nome']
         });
 
-        if (edvR)
-        {
+        if (edvR) {
+            administrador.destroy(
+                {
+                    where: { EDV: edvR }
+                });
+                
             return res.render('../views/usuarios_adm', { administradores });
 
         }
@@ -86,99 +93,106 @@ module.exports = {
 
     //direcionar para pagina login de adm
     async getPagLogin(req, res) {
-    res.render('../views/login-adm')
-},
+        res.render('../views/login-adm')
+    },
 
     // ^^-----------------------------------------------------------------------------------------------------------------------------------------
 
     //direcionar para pagina de cadastro para adm
     async getPagCadastro(req, res) {
-    res.render('../views/cad_adm', { nome: '', senha: '', edv: '' })
-},
+        res.render('../views/cad_adm', { nome: '', senha: '', edv: '' })
+    },
 
     // ^^-----------------------------------------------------------------------------------------------------------------------------------------
 
     //direcionar para pagina de home adm master
     async getPagHomeAdm(req, res) {
-    res.render('../views/home_adm_master')
-},
+        res.render('../views/home_adm_master')
+    },
 
     // ^^-----------------------------------------------------------------------------------------------------------------------------------------
 
     //cria um novo usuario no bd
     async postInsertUser(req, res) {
-    // Recebe as informações do front-end
-    const dados = req.body;
+        // Recebe as informações do front-end
+        const dados = req.body;
 
 
-    await administrador.create({
-        EDV: dados.edv,
-        Nome: dados.nome,
-        Senha: dados.senha
-    });
-    res.redirect('/');
-},
+        await administrador.create({
+            EDV: dados.edv,
+            Nome: dados.nome,
+            Senha: dados.senha
+        });
+        res.redirect('/');
+    },
 
     // ^^-----------------------------------------------------------------------------------------------------------------------------------------
 
     //direcionar para pagina que verifica adm (aceita/edita adm)
     async getUsuariosAp(req, res) {
 
-    const administradores = await administrador.findAll({
-        raw: true,
-        attributes: ['EDV', 'Nome']
-    })
-    res.render('../views/usuarios_adm', { administradores })
-},
+        const administradores = await administrador.findAll({
+            raw: true,
+            attributes: ['EDV', 'Nome'],
+            where: { Ativo: 0 }
+        })
+        res.render('../views/usuarios_adm', { administradores })
+    },
 
     // ^^-----------------------------------------------------------------------------------------------------------------------------------------
 
     //verifica se o login é valido 
     async verificaLogin(req, res) {
 
-    console.log(req.query)
-    
-    const dados = req.query;
-    const edv = dados.edv;
-    const senha = dados.senha;
+        console.log(req.query)
 
-    const user = await administrador.findOne({ where: { edv } });
+        const dados = req.query;
+        const edv = dados.edv;
+        const senha = dados.senha;
 
-    if (user == null) {
-        return res.sendStatus(400);
-    }
+        const user = await administrador.findOne({ where: { edv } });
 
-    if (user.Senha != senha) {
-        return res.sendStatus(400);
-    }
+        if (user == null) {
+            return res.sendStatus(400);
+        }
 
-    if (user.Ativo == 0) {
-        return res.sendStatus(400);
-    }
-    
-    const turma = await turmas.findAll({
-        raw: true,
-        attributes: ['ID', 'Nome', 'Inicio', 'Fim']
-    });
-    const declaracoe = await formulario.findAll({
-        raw: true,
-        attributes: ['ID', 'Nome', 'Inicio', 'IdAdmConferiu'],
-    });
+        if (user.Senha != senha) {
+            return res.sendStatus(400);
+        }
 
-    if (user.Master == 0) {
-        const id = user.EDV;
-        const declaracoes = await formulario.findAll({
+        if (user.Ativo == 0) {
+            return res.sendStatus(400);
+        }
+
+        const turma = await turmas.findAll({
+            raw: true,
+            attributes: ['ID', 'Nome', 'Inicio', 'Fim']
+        });
+        const declaracoe = await formulario.findAll({
             raw: true,
             attributes: ['ID', 'Nome', 'Inicio', 'IdAdmConferiu'],
-            where: { ID: id }
-        })
-        res.render('../views/declaracoes_adm', { turma, declaracoe, id });
-    }
-    else {
-        res.render('home_adm_master', { edvLogado: edv });
+        });
+
+        if (user.Master == 0) {
+            const id = user.EDV;
+            const declaracoes = await formulario.findAll({
+                raw: true,
+                attributes: ['ID', 'Nome', 'Inicio', 'IdAdmConferiu'],
+                where: { ID: id }
+            })
+            res.render('../views/declaracoes_adm', { turma, declaracoe, id });
+        }
+        else {
+            res.render('home_adm_master', { edvLogado: edv });
+        }
     }
 
-}
+    // ^^-----------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
 
 
 }
