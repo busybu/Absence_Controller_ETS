4a1635d7bd583ef3841382594ede13828e0eb737
Emanuelly <emanuellybs14@gmail.com>
2023-02-02 08:26:12 -0300

    aa

--- src/config/db.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require('sequelize');
-//configurações da base de dados
-const database = new sequelize('Absence_Controll', 'adm_absence_controll', 'etsds10243110',
-{
-dialect: 'mssql', host:'localhost', port: 49675
-});
-
-database.sync();
-module.exports = database;
