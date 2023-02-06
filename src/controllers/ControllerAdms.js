08adb2a0379689530ccd7aa57520f6bc0e75c1a1
busybu <gabrielle.fialkoski@hotmail.com>
2023-02-06 08:22:18 -0300

    fixed: bug da senha e design dos filtros

--- src/controllers/ControllerAdms.js
+++ src/controllers/ControllerAdms.js
@@ -240,7 +240,7 @@ module.exports = {
     //direcionar para pagina login de adm
     async getPagLogin(req, res) {
 
-        res.render('../views/login-adm', { verificar })
+        res.render('../views/login-adm', { verificar: ''})
     },
 
     // ^^-----------------------------------------------------------------------------------------------------------------------------------------
@@ -312,8 +312,9 @@ module.exports = {
         if (user == null || user.Senha != senha || user.Ativo == 0) {
             verificar = '0'
             return res.render('../views/login-adm', { verificar })
+            
         }
-
+        verificar = ''
         if (user.Master == 0) {
             const id = user.EDV;
             const declaracoe = await formulario.findAll({
